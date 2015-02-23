df5f8a364066a7dbce3404a29b4fe57521105847
kobilutil <kobilutil@users.noreply.github.com>
2015-02-23 22:24:31 +0200

    add tests

---
+++ tests/test.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+if "%1" == "" goto usage
+if "%2" == "" goto usage
+
+set num=%3
+if "%num%" == "" set num=100
+
+:loop
+<nul set /p =%1
+<nul 1>&2 set /p =%2
+<nul set /p =.
+
+set /a num=%num%-1
+if "%num%" == "0" goto :eof
+
+goto loop
+
+:usage
+echo USAGE: %~n0 ^<stdout text^> ^<stderr text^> [count] >&2
