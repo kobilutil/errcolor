801e49855e369590196d1faa5fe6b8d5ffb0fa90
kobilutil <kobilutil@users.noreply.github.com>
2015-02-24 23:17:59 +0200

    rename project to errcolor

--- errcolor/stdafx.cpp
+++ errcolor/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// stderr-colorer.pch will be the pre-compiled header
+// errcolor.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
